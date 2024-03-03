724852480b4291121dc58aeb1b3b46638067fdfc
MS71319 <115386655+MS71319@users.noreply.github.com>
2024-03-04 00:23:27 +0530

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -9,22 +9,18 @@ const {
   updateProduct,
   deleteProduct,
   addToWishlist,
-  uploadImages,
-  deleteImages
+
 } = require('../controller/productCtrl');
 
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
-const { uploadPhoto, productImgResize } = require("../middlewares/uploadImages");
 
 
 router.post("/", authMiddleware, isAdmin, createProduct);
-router.put("/upload/", authMiddleware, isAdmin, uploadPhoto.array("images", 10), productImgResize, uploadImages);
 router.get("/", getAllProduct);
 router.get("/:id", getaProduct);
 router.put("/wishlist", authMiddleware, addToWishlist); // This route seems different from the error message
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
-router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
 
 
 module.exports = router;
